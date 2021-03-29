3cd2533d0aea44f8d08358c65865d2be87d45a9d
Edoardo Cavazza <edoardo.cavazza@chialab.it>
2021-03-29 11:48:41 +0200

    feat: server and test runner and cli

---
+++ cli.js
@@ -0,0 +1,57 @@
+#!/usr/bin/env node
+
+const path = require('path');
+const { program } = require('commander');
+
+program
+    .version(require('./package.json').version);
+
+program
+    .command('build <entry>', { isDefault: true })
+    .description('build an entry')
+    .option('-O, --output <path>', 'output directory or file')
+    .option('-F, --format <type>', 'bundle format')
+    .option('-B, --bundle', 'bundle dependencies')
+    .option('-M, --minify', 'minify the build')
+    .action(async (input, { output, format = 'esm', bundle, minify, name }) => {
+        const { build } = require('./lib/index');
+        await build({
+            input: path.resolve(input),
+            output: path.resolve(output),
+            format,
+            name,
+            bundle,
+            minify,
+            sourcemap: true,
+        });
+    });
+
+program
+    .command('serve [root]')
+    .description('start the dev server')
+    .action(async (root) => {
+        const { serve } = require('./lib/index');
+        let config = {};
+        if (root) {
+            config.root = root;
+        }
+        await serve(config);
+    });
+
+program
+    .command('test <specs>')
+    .description('start the test runner')
+    .option('-W, --watch', 'watch test files')
+    .action(async (input, { watch }) => {
+        const { test } = require('./lib/index');
+        let config = {
+            watch,
+        };
+        if (input) {
+            config.files = [input];
+        }
+        await test(config);
+    });
+
+program
+    .parse(process.argv);

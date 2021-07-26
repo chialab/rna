861cb0587d5fc57c5ce1b59fcfcf431ada8a591a
Edoardo Cavazza <edoardo.cavazza@chialab.it>
2021-07-26 08:54:56 +0200

    chore: add module generator

---
+++ internals/generators/plopfile.cjs
@@ -0,0 +1,43 @@
+/**
+ * @param {import('plop').NodePlopAPI} plop
+ */
+module.exports = function(plop) {
+    plop.setGenerator('module', {
+        description: 'RNA ecosystem module.',
+        prompts: [{
+            type: 'input',
+            name: 'name',
+            message: 'name of the module:',
+        }, {
+            type: 'input',
+            name: 'description',
+            message: 'description of the module:',
+        }],
+        actions: [{
+            type: 'add',
+            path: '../../packages/{{dashCase name}}/package.json',
+            templateFile: './template/package.json.hbs',
+            abortOnFail: true,
+        }, {
+            type: 'add',
+            path: '../../packages/{{dashCase name}}/tsconfig.json',
+            templateFile: './template/tsconfig.json.hbs',
+            abortOnFail: true,
+        }, {
+            type: 'add',
+            path: '../../packages/{{dashCase name}}/README.md',
+            templateFile: './template/README.md.hbs',
+            abortOnFail: true,
+        }, {
+            type: 'add',
+            path: '../../packages/{{dashCase name}}/LICENSE.md',
+            templateFile: './template/LICENSE.hbs',
+            abortOnFail: true,
+        }, {
+            type: 'add',
+            path: '../../packages/{{dashCase name}}/lib/index.js',
+            templateFile: './template/index.js.hbs',
+            abortOnFail: true,
+        }],
+    });
+};

f0afaea92863da4159574f2a1b4d19481cecd771
Edoardo Cavazza <edoardo.cavazza@chialab.it>
2021-10-12 08:30:26 +0200

    fix: renable hmr cache

--- packages/rna-dev-server/lib/plugins/hmr.js
+++ packages/rna-dev-server/lib/plugins/hmr.js
@@ -1,12 +1,34 @@
 import { hmrPlugin as webHmrPlugin } from '@web/dev-server-hmr';
 
+/**
+ * @typedef {(path: string, module: any, needsReplacement: boolean) => void} SetNeedsReplacement
+ */
+
+/**
+ * @typedef {ReturnType<typeof webHmrPlugin> & { _setNeedsReplacement: SetNeedsReplacement }} HmrPlugin
+ */
+
+/**
+ *
+ * @returns
+ */
 export function hmrPlugin() {
-    const plugin = webHmrPlugin();
+    const plugin = /** @type {HmrPlugin} */ (webHmrPlugin());
+    const original = plugin._setNeedsReplacement;
+
     /**
-     * Sometimes the cache manager of the hmr plugin does not correctly update dependencies,
-     * so we are going to disable it.
+     * I did not find any reason why the needs replacement flag
+     * of a changed module should be resetted, so I am going to ignore reset requests
+     * since it causes bugs when a module is referenced across multiple files.
+     * @type {SetNeedsReplacement}
      */
-    plugin.transformCacheKey = undefined;
-    plugin.transformImport = undefined;
+    plugin._setNeedsReplacement = function(path, module, needsReplacement) {
+        if (!needsReplacement) {
+            return;
+        }
+
+        original.call(this, path, module, needsReplacement);
+    };
+
     return plugin;
 }

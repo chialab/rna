b67a6114320d214cd21b03324e680f5f08c38a33
Edoardo Cavazza <edoardo.cavazza@chialab.it>
2021-10-16 19:22:29 +0200

    fix: storybook typings

--- packages/rna-storybook/modules.d.ts
+++ packages/rna-storybook/modules.d.ts
@@ -4,6 +4,18 @@ declare module '@custom-elements-manifest/analyzer/src/create.js' {
     export function create(options: { modules: import('typescript').SourceFile[], plugins: any[] }): { modules: any[] };
 }
 
+declare module '@storybook/core-common' {
+    export interface StoriesSpecifier {
+        titlePrefix?: string;
+        directory: string;
+        files?: string;
+    }
+
+    export type NormalizedStoriesSpecifier = Required<StoriesSpecifier> & {
+        importPathMatcher: RegExp;
+    }
+}
+
 declare module '@storybook/core-server/dist/cjs/utils/StoryIndexGenerator.js' {
     import { Path } from '@storybook/store';
     import { NormalizedStoriesSpecifier } from '@storybook/core-common';

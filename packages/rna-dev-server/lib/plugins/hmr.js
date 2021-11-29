fcbe141a3e4b2e8e2c5b634be6ed7e5fc44adb8f
Edoardo Cavazza <edoardo.cavazza@chialab.it>
2021-11-29 09:33:12 +0100

    fix: hmr

--- packages/rna-dev-server/lib/plugins/hmr.js
+++ packages/rna-dev-server/lib/plugins/hmr.js
@@ -14,21 +14,13 @@ import { hmrPlugin as webHmrPlugin } from '@web/dev-server-hmr';
  */
 export function hmrPlugin() {
     const plugin = /** @type {HmrPlugin} */ (webHmrPlugin());
-    const original = plugin._setNeedsReplacement;
 
     /**
-     * I did not find any reason why the needs replacement flag
-     * of a changed module should be resetted, so I am going to ignore reset requests
+     * Disable the modified flag for a request
      * since it causes bugs when a module is referenced across multiple files.
      * @type {SetNeedsReplacement}
      */
-    plugin._setNeedsReplacement = function(path, module, needsReplacement) {
-        if (!needsReplacement) {
-            return;
-        }
-
-        original.call(this, path, module, needsReplacement);
-    };
+    plugin._setNeedsReplacement = function() {};
 
     return plugin;
 }

a5caaaf9f859141c1fd83b1bddf6120245822f8e
Edoardo Cavazza <edoardo.cavazza@chialab.it>
2022-04-13 14:14:38 +0200

    test: align result to esbuild update

--- packages/esbuild-plugin-meta-url/test/test.spec.js
+++ packages/esbuild-plugin-meta-url/test/test.spec.js
@@ -143,32 +143,27 @@ export {
 var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
 var __getOwnPropNames = Object.getOwnPropertyNames;
 var __hasOwnProp = Object.prototype.hasOwnProperty;
-var __markAsModule = (target) => __defProp(target, "__esModule", { value: true });
 var __export = (target, all) => {
   for (var name in all)
     __defProp(target, name, { get: all[name], enumerable: true });
 };
-var __reExport = (target, module2, copyDefault, desc) => {
-  if (module2 && typeof module2 === "object" || typeof module2 === "function") {
-    for (let key of __getOwnPropNames(module2))
-      if (!__hasOwnProp.call(target, key) && (copyDefault || key !== "default"))
-        __defProp(target, key, { get: () => module2[key], enumerable: !(desc = __getOwnPropDesc(module2, key)) || desc.enumerable });
+var __copyProps = (to, from, except, desc) => {
+  if (from && typeof from === "object" || typeof from === "function") {
+    for (let key of __getOwnPropNames(from))
+      if (!__hasOwnProp.call(to, key) && key !== except)
+        __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
   }
-  return target;
+  return to;
 };
-var __toCommonJS = /* @__PURE__ */ ((cache) => {
-  return (module2, temp) => {
-    return cache && cache.get(module2) || (temp = __reExport(__markAsModule({}), module2, 1), cache && cache.set(module2, temp), temp);
-  };
-})(typeof WeakMap !== "undefined" ? /* @__PURE__ */ new WeakMap() : 0);
+var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
 
 // test.spec.js
 var test_spec_exports = {};
 __export(test_spec_exports, {
   file: () => file
 });
-var file = new URL("./file.txt?emit=file", "file://" + __filename);
 module.exports = __toCommonJS(test_spec_exports);
+var file = new URL("./file.txt?emit=file", "file://" + __filename);
 // Annotate the CommonJS export names for ESM import in node:
 0 && (module.exports = {
   file

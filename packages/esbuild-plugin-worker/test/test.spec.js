9ec348990f7d8caebc2f2ce1f93263f20006d215
Edoardo Cavazza <edoardo.cavazza@chialab.it>
2022-04-13 14:26:54 +0200

    test: add worker tests for type module

--- packages/esbuild-plugin-worker/test/test.spec.js
+++ packages/esbuild-plugin-worker/test/test.spec.js
@@ -4,7 +4,7 @@ import workerPlugin from '@chialab/esbuild-plugin-worker';
 import { expect } from 'chai';
 
 describe('esbuild-plugin-worker', () => {
-    it('should load a classic worker', async () => {
+    it('should load a classic worker with bundle', async () => {
         const { outputFiles: [result, worker] } = await esbuild.build({
             absWorkingDir: new URL('.', import.meta.url).pathname,
             stdin: {
@@ -38,7 +38,40 @@ export {
         expect(path.dirname(result.path)).to.be.equal(path.dirname(worker.path));
     });
 
-    it('should load a module worker', async () => {
+    it('should load a classic worker without bundle', async () => {
+        const { outputFiles: [result, worker] } = await esbuild.build({
+            absWorkingDir: new URL('.', import.meta.url).pathname,
+            stdin: {
+                resolveDir: new URL('.', import.meta.url).pathname,
+                sourcefile: new URL(import.meta.url).pathname,
+                contents: 'export const worker = new Worker(\'./worker.js\');',
+            },
+            format: 'esm',
+            outdir: 'out',
+            bundle: false,
+            write: false,
+            plugins: [
+                workerPlugin(),
+            ],
+        });
+
+        expect(result.text).to.be.equal(`const worker = new Worker(new URL("./worker.js?emit=chunk", import.meta.url).href);
+export {
+  worker
+};
+`);
+        expect(worker.text).to.be.equal(`(() => {
+  // lib.worker.js
+  var postMessage = globalThis.postMessage;
+
+  // worker.js
+  postMessage("message");
+})();
+`);
+        expect(path.dirname(result.path)).to.be.equal(path.dirname(worker.path));
+    });
+
+    it('should load a module worker with bundle', async () => {
         const { outputFiles: [result, worker] } = await esbuild.build({
             absWorkingDir: new URL('.', import.meta.url).pathname,
             stdin: {
@@ -65,6 +98,36 @@ export {
 var postMessage = globalThis.postMessage;
 
 // worker.js
+postMessage("message");
+`);
+        expect(path.dirname(result.path)).to.be.equal(path.dirname(worker.path));
+    });
+
+    it('should load a module worker without bundle', async () => {
+        const { outputFiles } = await esbuild.build({
+            absWorkingDir: new URL('.', import.meta.url).pathname,
+            stdin: {
+                resolveDir: new URL('.', import.meta.url).pathname,
+                sourcefile: new URL(import.meta.url).pathname,
+                contents: 'export const worker = new Worker(\'./worker.js\', { type: "module" });',
+            },
+            format: 'esm',
+            outdir: 'out',
+            bundle: false,
+            write: false,
+            plugins: [
+                workerPlugin(),
+            ],
+        });
+
+        const [result, worker] = outputFiles;
+
+        expect(result.text).to.be.equal(`const worker = new Worker(new URL("./worker.js?emit=chunk", import.meta.url).href, { type: "module" });
+export {
+  worker
+};
+`);
+        expect(worker.text).to.be.equal(`import { postMessage } from "./lib.worker.js";
 postMessage("message");
 `);
         expect(path.dirname(result.path)).to.be.equal(path.dirname(worker.path));

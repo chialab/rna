a540fb9d27ecaf4aa3f289fb78107ec1f58d0edb
Edoardo Cavazza <edoardo.cavazza@chialab.it>
2022-05-02 21:48:59 +0200

    fix: use default React config when jsxModule is provided

--- packages/esbuild-plugin-jsx-import/lib/index.js
+++ packages/esbuild-plugin-jsx-import/lib/index.js
@@ -1,6 +1,9 @@
 import { createEmptySourcemapComment } from '@chialab/estransform';
 import { escapeRegexBody } from '@chialab/node-resolve';
 
+const DEFAULT_FACTORY = 'React.createElement';
+const DEFAULT_FRAGMENT = 'React.Fragment';
+
 /**
  * A plugin for esbuild that enables automatic injection of the jsx module import.
  * @param {{ jsxModule?: string, jsxExport?: 'named'|'namespace'|'default' }} opts
@@ -16,14 +19,20 @@ export default function(opts = {}) {
     const plugin = {
         name: 'jsx-import',
         setup(build) {
-            const { inject = [], jsxFactory, jsxFragment } = build.initialOptions;
+            const {
+                inject = [],
+                jsxFactory = DEFAULT_FACTORY,
+                jsxFragment = jsxFactory === DEFAULT_FACTORY ? DEFAULT_FRAGMENT : undefined,
+            } = build.initialOptions;
 
-            if (!jsxFactory || !opts || !opts.jsxModule) {
+            if (!opts || !opts.jsxModule) {
                 if (inject.includes(RUNTIME_ALIAS)) {
                     inject.splice(inject.indexOf(RUNTIME_ALIAS), 1);
                 }
                 return;
             }
+
+            const jsxExport = opts.jsxExport || jsxFactory === DEFAULT_FACTORY ? 'default' : undefined;
 
             if (!inject.includes(RUNTIME_ALIAS)) {
                 build.initialOptions.inject = [
@@ -40,9 +49,9 @@ export default function(opts = {}) {
 
             build.onLoad({ filter: RUNTIME_REGEX }, () => {
                 let contents = '';
-                if (opts.jsxExport === 'default') {
+                if (jsxExport === 'default') {
                     contents = `export { default as ${identifier} } from '${opts.jsxModule}';`;
-                } else if (opts.jsxExport === 'namespace') {
+                } else if (jsxExport === 'namespace') {
                     contents = `export * as ${identifier} from '${opts.jsxModule}';`;
                 } else {
                     contents = `export { ${specs.join(',')} } from '${opts.jsxModule}';`;

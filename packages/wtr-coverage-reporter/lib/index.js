ffd50297f5589ac7a32f8601cd7ceb8fc48767b8
Edoardo Cavazza <edoardo.cavazza@chialab.it>
2022-06-16 14:21:48 +0200

    feat: introducing the wtr coverage reporter

---
+++ packages/wtr-coverage-reporter/lib/index.js
@@ -0,0 +1,49 @@
+import { coverageReport, createCoverageReporter } from '@chialab/es-test-runner';
+
+/**
+ * Print coverage summary.
+ * @param {keyof import('@chialab/es-test-runner').ReportOptions} report
+ */
+export function coverageReporter(report = 'text-summary') {
+    /**
+     * @type {import('@web/test-runner-core').Logger}
+     */
+    let logger;
+
+    /**
+     * @type {import('@web/test-runner-core').Reporter}
+     */
+    const reporter = {
+        start({ config }) {
+            logger = config.logger;
+        },
+
+        onTestRunFinished({ testCoverage }) {
+            if (!testCoverage) {
+                return;
+            }
+
+            const context = coverageReport.createContext({
+                watermarks: {
+                    statements: [50, 80],
+                    functions: [50, 80],
+                    branches: [50, 80],
+                    lines: [50, 80],
+                },
+                coverageMap: testCoverage.coverageMap,
+            });
+            const writeFile = context.writer.writeFile;
+            context.writer.writeFile = function(file) {
+                const content = writeFile.call(this, file);
+                content.println = logger.log.bind(logger);
+
+                return content;
+            };
+
+            const reporter = createCoverageReporter(report);
+            reporter.execute(context);
+        },
+    };
+
+    return reporter;
+}

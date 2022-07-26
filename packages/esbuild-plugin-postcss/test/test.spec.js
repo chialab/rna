08dc2d5bc2be7d2d119f67249433c09c68029f1d
Edoardo Cavazza <edoardo.cavazza@chialab.it>
2022-07-26 15:54:16 +0200

    test: fix default postcss transformation

--- packages/esbuild-plugin-postcss/test/test.spec.js
+++ packages/esbuild-plugin-postcss/test/test.spec.js
@@ -26,7 +26,13 @@ describe('esbuild-plugin-postcss', () => {
 ::-webkit-input-placeholder {
   color: gray;
 }
+::-moz-placeholder {
+  color: gray;
+}
 :-ms-input-placeholder {
+  color: gray;
+}
+::-ms-input-placeholder {
   color: gray;
 }
 ::placeholder {

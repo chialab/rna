38b47e0045aee917563132402aa94a48074598bf
Edoardo Cavazza <edoardo.cavazza@chialab.it>
2023-01-11 17:48:03 +0100

    update esbuild

--- packages/esbuild-plugin-babel/test/test.spec.js
+++ packages/esbuild-plugin-babel/test/test.spec.js
@@ -74,13 +74,12 @@ import _regeneratorRuntime from "@babel/runtime/regenerator";
 var nil = /* @__PURE__ */ function() {
   var _ref = _asyncToGenerator(/* @__PURE__ */ _regeneratorRuntime.mark(function _callee() {
     return _regeneratorRuntime.wrap(function _callee$(_context) {
-      while (1) {
+      while (1)
         switch (_context.prev = _context.next) {
           case 0:
           case "end":
             return _context.stop();
         }
-      }
     }, _callee);
   }));
   return function nil2() {

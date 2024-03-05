faf6c384f0ec7ec8c36ab5c2ab5b10e1142590e0
Edoardo Cavazza <edoardo.cavazza@chialab.it>
2024-03-05 10:52:03 +0100

    Fix pdfjs test with node 21

--- packages/cjs-to-esm/test/fixtures/pdfjs.js
+++ packages/cjs-to-esm/test/fixtures/pdfjs.js
@@ -571,7 +571,7 @@ class FeatureTest {
     return shadow(this, "isOffscreenCanvasSupported", typeof OffscreenCanvas !== "undefined");
   }
   static get platform() {
-    if (typeof navigator === "undefined") {
+    if (typeof navigator === "undefined" || typeof navigator.platform !== "string") {
       return shadow(this, "platform", {
         isWin: false,
         isMac: false
@@ -15724,7 +15724,7 @@ exports.SVGGraphics = SVGGraphics;
 /************************************************************************/
 /******/ 	// The module cache
 /******/ 	var __webpack_module_cache__ = {};
-/******/ 	
+/******/
 /******/ 	// The require function
 /******/ 	function __w_pdfjs_require__(moduleId) {
 /******/ 		// Check if module is in cache
@@ -15738,14 +15738,14 @@ exports.SVGGraphics = SVGGraphics;
 /******/ 			// no module.loaded needed
 /******/ 			exports: {}
 /******/ 		};
-/******/ 	
+/******/
 /******/ 		// Execute the module function
 /******/ 		__webpack_modules__[moduleId](module, module.exports, __w_pdfjs_require__);
-/******/ 	
+/******/
 /******/ 		// Return the exports of the module
 /******/ 		return module.exports;
 /******/ 	}
-/******/ 	
+/******/
 /************************************************************************/
 var __webpack_exports__ = {};
 // This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
